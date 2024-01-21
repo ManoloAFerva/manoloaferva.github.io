47640dfad36a5a9855576420d0633f8765ae7f4e
Manolo A. Fernández Vargas <manolo.fernandez@Manolos-Mac-mini.local>
2024-01-21 04:03:13 -0500

    3D application rain for the Video Editing section

---
+++ scripts/importmap.js
@@ -0,0 +1,26 @@
+{
+  "imports": {
+    "lume": "https://ga.jspm.io/npm:lume@0.3.0-alpha.39/dist/index.js"
+  },
+  "scopes": {
+    "https://ga.jspm.io/": {
+      "@lume/autolayout": "https://ga.jspm.io/npm:@lume/autolayout@0.10.1/dist/AutoLayout.js",
+      "@lume/custom-attributes/dist/index.js": "https://ga.jspm.io/npm:@lume/custom-attributes@0.2.3/dist/index.js",
+      "@lume/element": "https://ga.jspm.io/npm:@lume/element@0.11.6/dist/index.js",
+      "@lume/eventful": "https://ga.jspm.io/npm:@lume/eventful@0.3.2/dist/index.js",
+      "@lume/kiwi": "https://ga.jspm.io/npm:@lume/kiwi@0.4.1/dist/kiwi.js",
+      "@lume/three-projected-material/dist/ProjectedMaterial.js": "https://ga.jspm.io/npm:@lume/three-projected-material@0.3.1/dist/ProjectedMaterial.js",
+      "classy-solid": "https://ga.jspm.io/npm:classy-solid@0.3.7/dist/index.js",
+      "element-behaviors": "https://ga.jspm.io/npm:element-behaviors@5.0.3/dist/index.js",
+      "james-bond": "https://ga.jspm.io/npm:james-bond@0.7.3/dist/index.js",
+      "lowclass": "https://ga.jspm.io/npm:lowclass@7.0.1/dist/index.js",
+      "regexr": "https://ga.jspm.io/npm:regexr@2.0.4/dist/index.js",
+      "solid-js": "https://ga.jspm.io/npm:solid-js@1.8.11/dist/dev.js",
+      "solid-js/html": "https://ga.jspm.io/npm:solid-js@1.8.11/html/dist/html.js",
+      "solid-js/store": "https://ga.jspm.io/npm:solid-js@1.8.11/store/dist/dev.js",
+      "solid-js/web": "https://ga.jspm.io/npm:solid-js@1.8.11/web/dist/dev.js",
+      "three": "https://ga.jspm.io/npm:three@0.160.0/build/three.module.js",
+      "three/": "https://ga.jspm.io/npm:three@0.160.0/"
+    }
+  }
+}
---
+++ scripts/lumeApp.js
@@ -0,0 +1,47 @@
+import * as lume from "lume";
+gsap.registerPlugin(ScrollTrigger);
+
+console.log(lume)
+
+var vpWidth = window.matchMedia("(max-width: 800px)")
+
+if (vpWidth.matches){
+	console.log("screen is small")
+	appRainScene.perspective = 1000;
+} else {console.log("sceen is big enough")}
+
+let appsTimeline = new gsap.timeline({
+	scrollTrigger: {
+		trigger: "#appsStart",
+		start: "top bottom",
+		endTrigger: "#appsEnd",
+		end: "center top",
+		scrub: 0,
+	}
+});
+
+appsTimeline.fromTo(premiere.rotation, {
+	x: -70,
+	y: 30,
+}, {
+	x: 10,
+	y: -45,
+})
+
+appsTimeline.fromTo(resolve.rotation, {
+ 	x: 90,
+    y: -10,
+    ease: "none",
+}, {
+	x: 0,
+	y: -30,
+}, "<");
+
+appsTimeline.fromTo(fcpx.rotation, {
+	x: -30,
+	y: -90,
+}, {
+	x: 45,
+	y: 45,
+}, "<")
+
