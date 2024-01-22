f22368a28686425a05183f6dc76c6c0c45177b6e
Manolo A. Fernández Vargas <manolo.fernandez@Manolos-Mac-mini.local>
2024-01-21 23:03:58 -0500

    Added App Rain animation for the Motion Graphics section

--- scripts/lumeApp.js
+++ scripts/lumeApp.js
@@ -6,13 +6,14 @@ var vpWidth = window.matchMedia("(max-width: 800px)")
 if (vpWidth.matches){
 	console.log("screen is small")
 	appRainScene.perspective = 1000;
+	mographRainScene.perspective = 1000;
 } else {console.log("sceen is big enough")}
 
 let appsTimeline = new gsap.timeline({
 	scrollTrigger: {
-		trigger: "#appsStart",
+		trigger: "#editStart",
 		start: "top bottom",
-		endTrigger: "#appsEnd",
+		endTrigger: "#editEnd",
 		end: "center top",
 		scrub: 0,
 	}
@@ -24,14 +25,14 @@ appsTimeline.fromTo(premiere.rotation, {
 }, {
 	x: 10,
 	y: -45,
-})
+});
 
 appsTimeline.fromTo(resolve.rotation, {
  	x: 90,
     y: -10,
     ease: "none",
 }, {
-	x: 0,
+	x: -45,
 	y: -30,
 }, "<");
 
@@ -41,5 +42,55 @@ appsTimeline.fromTo(fcpx.rotation, {
 }, {
 	x: 45,
 	y: 45,
+}, "<");
+
+// Motion Graphics App Rain Animation:
+
+let mographTimeline = new gsap.timeline({
+	scrollTrigger: {
+		trigger: "#mographRainStart",
+		start: "top bottom",
+		endTrigger: "#mographRainEnd",
+		end: "top top",
+		scrub: 0,
+	}
+});
+
+mographTimeline.fromTo(mographFollowLight.position, {
+	y: -400,
+}, {
+	y: 300,
+});
+
+mographTimeline.fromTo(after.rotation, {
+	x: 80,
+	y: -45,
+}, {
+	x: -80,
+	y: 45,
+}, "<");
+
+mographTimeline.fromTo(motn.rotation, {
+	x: 45,
+	y: -10,
+}, {
+	x: -15,
+	y: 10,
 }, "<")
+
+mographTimeline.fromTo(blender.rotation, {
+	x: -45,
+	y: 60,
+}, {
+	x: 15,
+	y: -30,
+}, "<");
+
+mographTimeline.fromTo(davinci.rotation, {
+	x: 140,
+	y: 30,
+}, {
+	x: -20,
+	y: -30,
+}, "<");
 
